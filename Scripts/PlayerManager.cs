b255d8d28bb505e74cc2f498571f03aaa95ec213
fossyman <stylesforrest@yahoo.co.uk>
2020-10-04 04:13:31 +0100

    Added more things(check desc for more)
    added berry bushed and interaction areas. also made the berry picking aninmation play when you interact around the bush. much more. cant remember. very late

--- Scripts/PlayerManager.cs
+++ Scripts/PlayerManager.cs
@@ -7,26 +7,57 @@ private Vector3 _Velocity;
 [Export]private float _Movespeed;
 private Camera _Camera;
 private CollisionShape PlayerCollision;
+private Area PlayerTriggerArea;
+private AnimationTree PlayerAnimationsTree;
+
+public bool CanPickBerries = false;
 
     public override void _Ready()
     {
         _Camera = (Camera)Owner.FindNode("Camera");
         PlayerCollision = (CollisionShape)FindNode("Collision");
+        PlayerTriggerArea = (Area)FindNode("Area");
+        PlayerTriggerArea.Connect("area_entered",this,"CheckTriggerAreaEnterCollision");
+        PlayerTriggerArea.Connect("area_exited",this,"CheckTriggerAreaExitCollision");
+        PlayerAnimationsTree = (AnimationTree)FindNode("AnimationTree");
     }
 
     public override void _Process(float delta)
    {
-      GetInputValues();
+      GetMovementInputValues();
       MoveAndSlide(_Velocity * _Movespeed);
    }
 
-    private void GetInputValues()
+    private void GetMovementInputValues()
     {
         _Velocity = Vector3.Zero;
         if(Input.IsActionPressed("move_forward")){ _Velocity.z += -_Movespeed; }
         if(Input.IsActionPressed("move_back")){ _Velocity.z += _Movespeed; }
         if(Input.IsActionPressed("move_left")){ _Velocity.x += -_Movespeed; }
         if(Input.IsActionPressed("move_right")){ _Velocity.x += _Movespeed; }
+        if(Input.IsActionJustPressed("interact")){ if(CanPickBerries){PlayerAnimationsTree.Set("parameters/OneShot/active",true);} }
+    }
+
+    public void CheckTriggerAreaEnterCollision(Area area)
+    {
+switch(area.CollisionMask)
+{
+    case 32:
+    CanPickBerries = true;
+                        GD.Print(CanPickBerries);
+    break;
+}
+    }
+
+        public void CheckTriggerAreaExitCollision(Area area)
+    {
+switch(area.CollisionMask)
+{
+    case 32:
+    CanPickBerries = false;
+                    GD.Print(CanPickBerries);
+    break;
+}
     }
 
 
